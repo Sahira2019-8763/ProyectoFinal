b62eafb3446ed47a38ad2880995078b8a792d6d9
sahira <sahirareyesmedina@gmail.com>
2020-07-26 15:01:20 -0400

    proyecto final

---
+++ Procedimiento.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal
+{
+   public class Procedimiento
+    {
+        static public void Mostrar(long us1, long us2, long us3, long us4, long us5, int c, int c1, int c2, int c3, int c4, int c5)
+
+        {
+            Console.WriteLine("1- ESCRIBA SU NOMBRE ");
+            string n = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("2- INGRESE SU USUARIO");
+            long us = Convert.ToInt64(Console.ReadLine());
+
+            if ((us == us1) || (us == us2) || (us == us3) || (us == us4) || (us == us5))
+            {
+                Console.WriteLine("3- INGRESE SU CLAVE");
+                c = Convert.ToInt32(Console.ReadLine());
+            }
+
+            else if ((us != us1) || (us != us2) || (us != us3) || (us != us4) || (us != us5))
+            {
+                while (us != us1 && us != us2 && us != us3 && us != us4 && us != us5)
+                {
+                    Console.WriteLine("¡USUARIO INCORRECTO! ");
+                    Console.WriteLine("DIGITE OTRO USUARIO");
+                    us = Convert.ToInt64(Console.ReadLine());
+                }
+                Console.WriteLine("INGRESE SU CLAVE:");
+                c = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if ((c != c1) && (c != c2) && (c != c3) && (c != c4) && (c != c5))
+            {
+                do
+                {
+                    Console.WriteLine("¡CONTRASEÑA INCORRECTA!");
+                    Console.WriteLine("INGRESE SU CLAVE:");
+                    c = Convert.ToInt32(Console.ReadLine());
+                }
+
+                while ((c != c1) && (c != c2) && (c != c3) && (c != c4) && (c != c5));
+            }
+            else if ((c == c4 && us == us4) || (c == c5 && us == us5))
+            {
+                do
+                {
+                    Console.WriteLine("ESTE USUARIO ESTÁ INACTIVO, POR FAVOR VUELVA Y VALIDE OTRO USUARIO.");
+                    Console.WriteLine("INGRESE OTRO USUARIO");
+                    us = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("INGRESE SU CLAVE");
+                    c = Convert.ToInt32(Console.ReadLine());
+                }
+
+                while (c == c4 && us == us4 || c == c5 && us == us5);
+            }
+
+            if ((c == c1) && (us == us1))
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("                    ESTADO ACTIVO");
+                Console.WriteLine(" ");
+                Console.WriteLine("La fecha de creacion de este usuario es: " + Procedimiento.fecha());
+                Console.WriteLine("Bienvenido " + n.ToUpper() + ", usted ingreso con el USUARIO 1. Su rol es el de ADMINISTRADOR");
+                Console.WriteLine("NO HAY ASIGNACIONES PENDIENTES, PUEDE RETIRARSE");
+            }
+            else if ((c == c2) && (us == us2))
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("                    ESTADO ACTIVO");
+                Console.WriteLine(" ");
+                Console.WriteLine("La fecha de creacion de este usuario es: " + Procedimiento.fecha2());
+                Console.WriteLine("Bienvenido " + n.ToUpper() + ", usted ingreso con el USUARIO 2. Su rol es el de SUPERVISOR");
+                Console.WriteLine("NO HAY SUPERVISIONES PENDIENTES, PUEDE RETIRARSE");
+            }
+            else if ((c == c3) && (us == us3))
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("                    ESTADO ACTIVO");
+                Console.WriteLine(" ");
+                Console.WriteLine("La fecha de creacion de este usuario es: " + Procedimiento.fecha3());
+                Console.WriteLine("Bienvenido " + n.ToUpper() + ", usted ingreso con el USUARIO 3. Su rol es el de VENDEDOR");
+                Console.WriteLine("NO HAY VENTAS PENDIENTES, PUEDE RETIRARSE");
+            }
+
+            else
+            {
+                Console.WriteLine("LA CONTRASEÑA QUE ESCRIBIÓ NO COINCIDE CON EL USUARIO INGRESADO");
+                Console.WriteLine("¡Hasta luego! vuelva cuando sepa la contraseña de SU USUARIO");
+            }
+        }
+
+        public static DateTime fecha()
+        {
+            DateTime fecha1 = new DateTime(2019, 11, 23);
+            return fecha1;
+        }
+
+        public static DateTime fecha2()
+        {
+            DateTime fecha2 = new DateTime(2019, 11, 18);
+            return fecha2;
+        }
+        public static DateTime fecha3()
+        {
+            DateTime fecha3 = new DateTime(2020, 03, 15);
+            return fecha3;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long us1 = 40211111111, us2 = 40222222222, us3 = 40233333333, us4 = 40244444444, us5 = 40255555555;
+            int clave1 = 12345, clave2 = 54321, clave3 = 12354, clave4 = 54312, clave5 = 43251, clave = 0;
+            Console.WriteLine("                        INICIAR SESION");
+            Console.WriteLine(" ");
+            Console.WriteLine("            USUARIOS PARA ELEGIR");
+            Console.WriteLine(" ");
+            Program.Saludo(us1, clave1, 1);
+            Program.Saludo(us2, clave2, 2);
+            Program.Saludo(us3, clave3, 3);
+            Program.Saludo(us4, clave4, 4);
+            Program.Saludo(us5, clave5, 5);
+            Console.WriteLine(" ");
+            Procedimiento.Mostrar(us1, us2, us3, us4, us5, clave, clave1, clave2, clave3, clave4, clave5);
+
+            Console.ReadKey();
+        }
+        static void Saludo(long usuari, int clav, int cont)
+        {
+            Console.WriteLine(" Usuario " + cont + " = " + usuari + "      clave = " + clav);
+        }
+    }
+}
